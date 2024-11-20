7fed42b0d3f5bfb6f43002eab2cae4d3b701510b
Alex Chubaty <achubaty@for-cast.ca>
2024-11-19 23:24:49 -0700

    pkg updates

--- _TODO.R
+++ _TODO.R
@@ -10,23 +10,23 @@ renv::install(c(
 
 workflowtools::setup_project() ## done
 
-source("R/project_description.R") ## done; will need to be updated
+source("R/project_description.R") ## done; will be updated again below
 
 usethis::use_readme_md() ## done
 
 ## 2. add LandR Biomass modules as git submodules
-workflowtools::use_module("modules", "PredictiveEcology/Biomass_borealDataPrep@development") ## done
-workflowtools::use_module("modules", "PredictiveEcology/Biomass_core@development") ## done
-workflowtools::use_module("modules", "PredictiveEcology/Biomass_speciesData@development") ## done
-workflowtools::use_module("modules", "PredictiveEcology/Biomass_speciesParameters@development") ## done
+workflowtools::use_module("PredictiveEcology/Biomass_borealDataPrep@development", path = "modules") ## done
+workflowtools::use_module("PredictiveEcology/Biomass_core@development", path = "modules") ## done
+workflowtools::use_module("PredictiveEcology/Biomass_speciesData@development", path = "modules") ## done
+workflowtools::use_module("PredictiveEcology/Biomass_speciesParameters@development", path = "modules") ## done
 
 ## 3. create new modules for SBW and add as git submodules
 ## 3a. create empty repos on GitHub.com and add each as submodule
-workflowtools::use_module("modules", "SBW_dataPrep") ## done
-workflowtools::use_module("modules", "SBW_recruitment") ## done
-workflowtools::use_module("modules", "SBW_dispersal") ## done
-workflowtools::use_module("modules", "SBW_naturalEnemies") ## done
-workflowtools::use_module("modules", "SBW_defoliation") ## done
+workflowtools::use_module("FOR-CAST/SBW_dataPrep", path = "modules") ## done
+workflowtools::use_module("FOR-CAST/SBW_recruitment", path = "modules") ## done
+workflowtools::use_module("FOR-CAST/SBW_dispersal", path = "modules") ## done
+workflowtools::use_module("FOR-CAST/SBW_naturalEnemies", path = "modules") ## done
+workflowtools::use_module("FOR-CAST/SBW_defoliation", path = "modules") ## done
 
 ## 3b. create new module for each using template
 SpaDES.core::newModule("SBW_dataPrep", path = "modules") ## done
@@ -41,9 +41,13 @@ SpaDES.core::newModule("SBW_recruitment", path = "modules") ## done
 ## 3d. ensure absolute paths in test files converted to relative ones
 ## done
 
-## 4. copy over code from Trisect modules and add to SBW ones
+## 4. update packages based on modules
+workflowtools::check_project_packages()
+source("R/project_description.R")
+
+## 5. copy over code from Trisect modules and add to SBW ones
 ## TODO
 
-## 5. create new R package (SBW_utils) based on TriSectUtils
+## 6. create new R package (SBW_utils) based on TriSectUtils
 ## TODO
 
